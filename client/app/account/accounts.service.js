1cd891fd66c9158db62876ee0a56f690121fcb6f
Carlos Ramos <carragom@gmail.com>
2017-04-10 11:40:31 -0600

    Add missing file from previous commit

---
+++ client/app/account/accounts.service.js
@@ -0,0 +1,26 @@
+function AdminResourceBuilder(collection) {
+   return function AdminResource($resource) {
+      var root = '/api/admin';
+      var itemUrl = root + '/:collection/:id';
+      var searchUrl = root + '/:collection/search/:search';
+
+      return $resource(itemUrl, { id: '@id', collection: collection }, {
+         query: {
+            isArray: false,
+         },
+         update: {
+            url: itemUrl,
+            method: 'PUT',
+         },
+         search: {
+            url: searchUrl,
+            method: 'GET',
+         }
+      });
+   }
+}
+
+angular
+   .module('components.account')
+   .factory('Account', AdminResourceBuilder('users'))
+   .factory('Role', AdminResourceBuilder('roles'));
